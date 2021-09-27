0ca37636945a70eaa9b5e2fbc57e6bb8591887ee
BA1bers <brent.albers@yahoo.com>
2021-09-27 01:08:35 -0700

    added gitignore, updated package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+// import inquirer package
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHtmlTemplate = require('./src/TemplatePage');
+
+// Require Employees
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
